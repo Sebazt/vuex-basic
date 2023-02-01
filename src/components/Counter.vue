2f8695aaf224ffb715db29f1ec8116071a0bfa4e
Sebazt <juan.londono29@udea.edu.co>
2023-01-31 20:45:32 -0500

    :rocket: init  de project

---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Counter - vuex</h1>
+  <h2>Direct Acces: {{ $store.state.count }}</h2>
+   <h2>Computed: {{ countComputed }}</h2>
+
+  <button @click="increment">Increment +1 </button>
+  <button @click="incrementBy">Increment +5 </button>
+  <button @click="incrementRandomInt">Random </button>
+  <h1>Map State</h1>
+  <h2>MapState: {{ count }}</h2>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+
+  computed: {
+    countComputed() {
+      return this.$store.state.count;
+    },
+    ...mapState({
+      count: state => state.count
+    })
+  },
+
+  methods:{
+    increment(){
+      this.$store.commit('increment')
+    },
+    incrementBy(){
+      this.$store.commit('incrementBy', 5)
+    },
+    incrementRandomInt(){
+      this.$store.dispatch('incrementRandomInt')
+    }
+  }
+};
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import { createStore } from 'vuex';
+import getRandomInt from '../helper/getRandomInt'
+
+export default createStore({
+  state: {
+    count: 1,
+    lastMutation: 'none',
+    isLoading:false
+  },
+
+  mutations: {
+
+    increment(state) {
+      state.count++
+      state.lastMutation = 'increment'
+    },
+
+    incrementBy(state, val) {
+      state.count+= val
+      state.lastMutation = 'incrementBy'
+    },
+  },
+
+  actions:{
+    async incrementRandomInt( context ){
+      const randomInt = await getRandomInt();
+      context.commit('incrementBy', randomInt)
+    }
+  }
+})
